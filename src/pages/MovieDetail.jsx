94e2037a473367682ba97373f8ca46c0b03a8aad
heejin31 <heejin0229@hanmail.net>
2022-12-16 16:29:50 +0900

    첫업로드

---
+++ src/pages/MovieDetail.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { movieAction } from "../redux/action/movieAction";
+import MoonLoader from "react-spinners/MoonLoader";
+import MovieExplain from "../components/MovieExplain";
+import Footer from "../components/Footer";
+
+const MovieDetail = () => {
+  const { id } = useParams();
+  const { detailMovies, loading, trailerVideo } = useSelector(
+    (state) => state.movie,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(movieAction.getDetailMovies(id));
+    window.scrollTo(0, 0); //화면 제일 위로 올라간 상태로 열리게
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <MoonLoader color="white" loading={loading} size={150} />
+      </div>
+    );
+  }
+  return (
+    <div>
+      {console.log("detailMovies는?", detailMovies)}
+      <MovieExplain item={detailMovies} videoId={trailerVideo} />
+      <Footer />
+    </div>
+  );
+};
+
+export default MovieDetail;
